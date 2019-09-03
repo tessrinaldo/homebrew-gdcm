68933c3d7ffb518a89199bf0ce7c2762d53a2ee1
tessrinaldo <tessrinaldo@gmail.com>
2019-09-03 14:12:37 -0700

    fix spacing, add sha

--- gdcm.rb
+++ gdcm.rb
@@ -1,71 +1,72 @@
 class Gdcm < Formula
-    desc "Grassroots DICOM library and utilities for medical files, with commandline tools"
-    homepage "https://sourceforge.net/projects/gdcm/"
-    url "https://github.com/malaterre/GDCM/archive/v3.0.1.tar.gz"
+  desc "Grassroots DICOM library and utilities for medical files, with command line tool"
+  homepage "https://sourceforge.net/projects/gdcm/"
+  url "https://github.com/malaterre/GDCM/archive/v3.0.1.tar.gz"
+  sha256 "f1ee8ebda7a465281abada329b4dbca6e036a42ead6ad58070ff4f94da7819d9"
 
-    bottle do
-        sha256 "02acc5b9d928de052b0a1ea8ce06c0184ad573d86797694f408d83c185d43624" => :mojave
-        sha256 "fce24222b4aa0414b4acaee2622ecb2005bd048bc8d95e11a6e1a0a46f0252b6" => :high_sierra
-        sha256 "05b046743c582b5bb7b604538e82e2c7a1c33caee2938c5a449716c404d1659a" => :sierra
+  bottle do
+    sha256 "02acc5b9d928de052b0a1ea8ce06c0184ad573d86797694f408d83c185d43624" => :mojave
+    sha256 "fce24222b4aa0414b4acaee2622ecb2005bd048bc8d95e11a6e1a0a46f0252b6" => :high_sierra
+    sha256 "05b046743c582b5bb7b604538e82e2c7a1c33caee2938c5a449716c404d1659a" => :sierra
+  end
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "swig" => :build
+  depends_on "openjpeg"
+  depends_on "openssl"
+  depends_on "python"
+  depends_on "vtk"
+
+  def install
+    ENV.cxx11
+
+    xy = Language::Python.major_minor_version "python3"
+    python_include = Utils.popen_read("python3 -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))'").chomp
+    python_executable = Utils.popen_read("python3 -c 'import sys;print(sys.executable)'").chomp
+
+    args = std_cmake_args + %W[
+      -GNinja
+      -DGDCM_BUILD_APPLICATIONS=OFF
+      -DGDCM_BUILD_SHARED_LIBS=ON
+      -DGDCM_BUILD_TESTING=OFF
+      -DGDCM_BUILD_EXAMPLES=OFF
+      -DGDCM_BUILD_DOCBOOK_MANPAGES=OFF
+      -DGDCM_USE_VTK=ON
+      -DGDCM_USE_SYSTEM_OPENJPEG=ON
+      -DGDCM_USE_SYSTEM_OPENSSL=ON
+      -DGDCM_WRAP_PYTHON=ON
+      -DPYTHON_EXECUTABLE=#{python_executable}
+      -DPYTHON_INCLUDE_DIR=#{python_include}
+      -DGDCM_INSTALL_PYTHONMODULE_DIR=#{lib}/python#{xy}/site-packages
+      -DCMAKE_INSTALL_RPATH=#{lib}
+      -DGDCM_NO_PYTHON_LIBS_LINKING=ON
+    ]
+
+    mkdir "build" do
+      ENV.append "LDFLAGS", "-undefined dynamic_lookup"
+
+      system "cmake", "..", *args
+      system "ninja"
+      system "ninja", "install"
     end
+  end
 
-    depends_on "cmake" => :build
-    depends_on "ninja" => :build
-    depends_on "pkg-config" => :build
-    depends_on "swig" => :build
-    depends_on "openjpeg"
-    depends_on "openssl"
-    depends_on "python"
-    depends_on "vtk"
+  test do
+    (testpath/"test.cxx").write <<~EOS
+      #include "gdcmReader.h"
+      int main(int, char *[])
+      {
+        gdcm::Reader reader;
+        reader.SetFileName("file.dcm");
+      }
+    EOS
 
-    def install
-        ENV.cxx11
-    
-        xy = Language::Python.major_minor_version "python3"
-        python_include = Utils.popen_read("python3 -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))'").chomp
-        python_executable = Utils.popen_read("python3 -c 'import sys;print(sys.executable)'").chomp
-    
-        args = std_cmake_args + %W[
-          -GNinja
-          -DGDCM_BUILD_APPLICATIONS=ON
-          -DGDCM_BUILD_SHARED_LIBS=ON
-          -DGDCM_BUILD_TESTING=OFF
-          -DGDCM_BUILD_EXAMPLES=OFF
-          -DGDCM_BUILD_DOCBOOK_MANPAGES=OFF
-          -DGDCM_USE_VTK=ON
-          -DGDCM_USE_SYSTEM_OPENJPEG=ON
-          -DGDCM_USE_SYSTEM_OPENSSL=ON
-          -DGDCM_WRAP_PYTHON=ON
-          -DPYTHON_EXECUTABLE=#{python_executable}
-          -DPYTHON_INCLUDE_DIR=#{python_include}
-          -DGDCM_INSTALL_PYTHONMODULE_DIR=#{lib}/python#{xy}/site-packages
-          -DCMAKE_INSTALL_RPATH=#{lib}
-          -DGDCM_NO_PYTHON_LIBS_LINKING=ON
-        ]
-    
-        mkdir "build" do
-          ENV.append "LDFLAGS", "-undefined dynamic_lookup"
-    
-          system "cmake", "..", *args
-          system "ninja"
-          system "ninja", "install"
-        end
-      end
-    
-      test do
-        (testpath/"test.cxx").write <<~EOS
-          #include "gdcmReader.h"
-          int main(int, char *[])
-          {
-            gdcm::Reader reader;
-            reader.SetFileName("file.dcm");
-          }
-        EOS
-    
-        system ENV.cxx, "-std=c++11", "-isystem", "#{include}/gdcm-3.0", "-o", "test.cxx.o", "-c", "test.cxx"
-        system ENV.cxx, "-std=c++11", "test.cxx.o", "-o", "test", "-L#{lib}", "-lgdcmDSED"
-        system "./test"
-    
-        system "python3", "-c", "import gdcm"
-      end
-    end
+    system ENV.cxx, "-std=c++11", "-isystem", "#{include}/gdcm-3.0", "-o", "test.cxx.o", "-c", "test.cxx"
+    system ENV.cxx, "-std=c++11", "test.cxx.o", "-o", "test", "-L#{lib}", "-lgdcmDSED"
+    system "./test"
+
+    system "python3", "-c", "import gdcm"
+  end
+end
