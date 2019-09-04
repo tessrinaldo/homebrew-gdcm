18f577f1813cff4a39d0e25936089db54b27678b
tessrinaldo <tessrinaldo@gmail.com>
2019-09-03 17:37:36 -0700

    add version release bottle

--- gdcm-with-tool.rb
+++ gdcm-with-tool.rb
@@ -4,6 +4,11 @@ class GdcmWithTool < Formula
   url "https://github.com/malaterre/GDCM/archive/v3.0.1.tar.gz"
   sha256 "f1ee8ebda7a465281abada329b4dbca6e036a42ead6ad58070ff4f94da7819d9"
 
+  bottle do
+    root_url "https://github.com/tessrinaldo/homebrew-gdcm/releases/v3.0.1"
+    sha256 "23654fa651cae21219d5896a1f01ad3d52d6953599528a656005d739d718f5ad" => :mojave
+  end
+
   depends_on "cmake" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
